5c3758d6aca137d1cf2e201b7b71c69679b8bf12
mahdi-kheibari <mahdi.khaybari@gmail.com>
2023-03-10 15:36:20 +0330

    custom material theme was added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
 import './App.css';
 
 function App() {
-  return (<></>);
+    return (<ThemeProvider theme={theme}></ThemeProvider>);
 }
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,93 @@
+import { createTheme } from '@mui/material/styles';
+
+const typographyVariants = {
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    fontWeightBold: 500,
+    fontWeightMedium: 400,
+    fontWeightRegular: 300,
+    fontWeightLight: 300,
+}
+const theme = createTheme({
+    palette: {
+        primary: { main: '#0cae96' },
+        primary_light: { main: '#6cfbdb' },
+        secondary: { main: '#28272c' },
+        success: { main: '#22C157' },
+        info: { main: '#6072FB' },
+        warning: { main: '#fd9350' },
+        danger: { main: '#FB6072' },
+        gray: { main: '#79797a' },
+        gray_light: { main: '#bcbcbd' },
+        light: { main: '#fefefe' },
+        dark: { main: '#2c3e50' },
+        white: { main: '#fff' },
+        muted: { main: '#6c757d' }
+    },
+    spacing: (factor) => `${0.25 * factor}rem`,
+    breakpoints: {
+        keys: ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'],
+        values: {
+            xs: 0,
+            sm: 576,
+            md: 768,
+            lg: 992,
+            xl: 1200,
+            xxl: 1400
+        },
+    },
+    typography: { ...typographyVariants },
+    components: {
+        MuiContainer: {
+            styleOverrides: {
+                maxWidthSm: {
+                    '&.MuiContainer-maxWidthSm': {
+                        maxWidth: '540px',
+                    },
+                },
+                maxWidthMd: {
+                    '&.MuiContainer-maxWidthMd': {
+                        maxWidth: '720px',
+                    },
+                },
+                maxWidthLg: {
+                    '&.MuiContainer-maxWidthLg': {
+                        maxWidth: '960px',
+                    },
+                },
+                maxWidthXl: {
+                    '&.MuiContainer-maxWidthXl': {
+                        maxWidth: '1140px',
+                    },
+                },
+                maxWidthXxl: {
+                    '&.MuiContainer-maxWidthXxl': {
+                        maxWidth: '1320px',
+                    },
+                },
+                fixed: {
+                    '&.MuiContainer-fixed': {
+                    },
+                }
+            }
+        },
+    },
+});
+const ContainerFixed = {
+    [theme.breakpoints.up('sm')]: {
+        maxWidth: '540px',
+    },
+    [theme.breakpoints.up('md')]: {
+        maxWidth: '720px',
+    },
+    [theme.breakpoints.up('lg')]: {
+        maxWidth: '960px',
+    },
+    [theme.breakpoints.up('xl')]: {
+        maxWidth: '1140px',
+    },
+    [theme.breakpoints.up('xxl')]: {
+        maxWidth: '1320px',
+    },
+}
+theme.components.MuiContainer.styleOverrides.fixed['&.MuiContainer-fixed'] = ContainerFixed;
+export default theme;
