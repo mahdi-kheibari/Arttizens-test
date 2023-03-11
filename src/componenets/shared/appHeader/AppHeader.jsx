33a872f59cd4aa958b2e5c9ec255f8462643bfb4
mahdi-kheibari <mahdi.khaybari@gmail.com>
2023-03-12 02:06:54 +0330

    header was completed

--- src/componenets/shared/appHeader/AppHeader.jsx
+++ src/componenets/shared/appHeader/AppHeader.jsx
@@ -1,8 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, Link, Button, IconButton } from "@mui/material";
 import React, { useContext } from "react";
 import { store } from "../../../store/Context";
 import ListItems from "../../listItems/ListItems";
 import PopoverItem from "../../listItems/popoverItem/PopoverItem";
+import VolumeUpOutlinedIcon from "@mui/icons-material/VolumeUpOutlined";
+import NightlightOutlinedIcon from "@mui/icons-material/NightlightOutlined";
+import CloudDownloadOutlinedIcon from "@mui/icons-material/CloudDownloadOutlined";
+import LanguageIcon from "@mui/icons-material/Language";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import "./appHeader.scss";
 
 const AppHeader = () => {
   const { headerItems } = useContext(store);
@@ -28,7 +34,7 @@ const AppHeader = () => {
             mr: "10px",
           }}
         ></Box>
-        <Box sx={{ display: "flex" }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           {headerItems.map((item) => (
             <ListItems
               key={item.text ? item.text : item}
@@ -45,9 +51,67 @@ const AppHeader = () => {
                 : null}
             </ListItems>
           ))}
+          <Link
+            component="button"
+            underline="none"
+            className="zone-btn font-14"
+          >
+            Newbie Zone
+          </Link>
         </Box>
       </Box>
-      <div></div>
+      <Box display={"flex"}>
+        <Box sx={{ display: "flex", mx: "20px" }}>
+          <Button
+            variant="text"
+            color="secondary"
+            sx={{
+              textTransform: "unset",
+              "&:hover": { color: "primary.main" },
+            }}
+          >
+            Login
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ textTransform: "unset", color: "white.main", p: "4px 24px" }}
+            disableElevation
+          >
+            Sign Up
+          </Button>
+        </Box>
+        <IconButton
+          sx={{ "&:hover": { color: "primary.main" } }}
+          aria-label="report"
+        >
+          <VolumeUpOutlinedIcon fontSize="small" />
+        </IconButton>
+        <IconButton
+          sx={{ "&:hover": { color: "primary.main" } }}
+          aria-label="theme"
+        >
+          <NightlightOutlinedIcon fontSize="small" />
+        </IconButton>
+        <IconButton
+          sx={{ "&:hover": { color: "primary.main" } }}
+          aria-label="download"
+        >
+          <CloudDownloadOutlinedIcon fontSize="small" />
+        </IconButton>
+        <Button
+          variant="text"
+          sx={{
+            "&:hover": { color: "primary.main" },
+            color: "gray.main",
+            textTransform: "unset",
+          }}
+        >
+          <LanguageIcon fontSize="small" sx={{ mx: 1 }} />
+          {"/USD"}
+          <KeyboardArrowDownIcon fontSize="small" />
+        </Button>
+      </Box>
     </Box>
   );
 };
